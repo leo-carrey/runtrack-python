f396cd2535bb4d4844057002523fd0272b1723d4
leo-carrey <leo.carrey@laplateforme.io>
2023-01-02 17:00:40 +0100

    [ADD] 1st commit

---
+++ jour01/file.py
@@ -0,0 +1,18 @@
+# Create a program where you enter a birth year and it returns your actual age
+
+# def guess_age(birthyear):
+#     res=2023-birthyear
+#     return res
+
+# print(guess_age(1999))
+
+#Create a program that takes a number of students, and determines how many groups of 5 can be made
+
+def group(students,n):
+    res=students//n
+    return res
+
+print(group(158,50))
+
+
+
