40829e33610f1e377616f335aa50558dfaa6273a
leo-carrey <leo.carrey@laplateforme.io>
2023-01-09 09:16:56 +0100

    [ADD] 5th commit

---
+++ jour05/job01/job01.py
@@ -0,0 +1,5 @@
+def helloxx():
+    name = input("please enter your name: ")
+    return 'hello '+name
+
+print(helloxx())
---
+++ jour05/job02/job02.py
@@ -0,0 +1,15 @@
+def draw_rectangle(w,h):
+    print("|", "-" * (w - 2), "|")
+    for i in range (h - 2):
+        print("|", " " * (w - 2), "|")
+    print("|", "-" * (w - 2), "|")
+
+draw_rectangle(10,3)
+
+
+# w = int(input('Please Enter the height of the Rectangle: '))
+# h = int(input('Please Enter the Width of the Rectangle: '))
+# print("|", "-" * (w - 2), "|")
+# for i in range (h - 2):
+#     print("|", " " * (w - 2), "|")
+# print("|", "-" * (w - 2), "|")
